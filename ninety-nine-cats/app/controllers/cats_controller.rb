2830f293687392952a4bf5e8897e1fd2a98f59a8
claudiaaziz <claudiahannahh@gmail.com>
2023-09-29 16:14:08 -0400

    Add cats controller

--- ninety-nine-cats/app/controllers/cats_controller.rb
+++ ninety-nine-cats/app/controllers/cats_controller.rb
@@ -1,2 +1,37 @@
 class CatsController < ApplicationController
+    def index
+        cats = Cat.all
+        cats_url
+    end
+
+    def new
+        render :new
+    end
+
+    def create
+        new_cat = Cat.new(cat_params)
+        if new_cat.save
+            redirect_to cat_url(new_cat)
+        else
+            render :new
+        end
+    end
+
+    def edit
+        render :edit
+    end
+
+    def update
+        cat = Cat.find(params[:id])
+        if cat && cat.update(cat_params)
+            redirect_to cat_url(cat)
+        else
+            render :edit
+        end
+    end
+
+    def cat_params
+        params.require(:cat).permit(:color, :birth_date, :name, :sex, :description)
+    end
+
 end
